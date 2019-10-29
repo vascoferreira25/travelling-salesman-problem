e51e0bf10b277dd584cd30071287b601d9567e9c
Vasco Ferreira <vasco_mmf@hotmail.com>
2019-10-29 14:06:55 +0000

    Fixed project.clj and generated documentation

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject travelling-salesman-problem "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject travelling-salesman-problem "1.0"
+  :description "Implementation of a Genetic Algorithm to solve the Travelling Salesman Problem."
+  :url "https://github.com/vascoferreira25/travelling-salesman-problem"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [uncomplicate/neanderthal "0.26.1"]
-                 [criterium "0.4.4"]]
+                 [uncomplicate/neanderthal "0.26.1"]]
   :plugins [[lein-marginalia "0.9.1"]]
   :main ^:skip-aot travelling-salesman-problem.core
   :target-path "target/%s"
